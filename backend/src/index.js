fb7264c0de993411c0704944648a0cbcce51e73c
Robert-29 <robert292005@gmail.com>
2026-09-21 10:50:21 -0600

    Prestación para AX

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,11 @@ const PORT = process.env.PORT || 3001;
 
 // Middlewares globales
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: true,
   credentials: true
 }));
+
+
 app.use(express.json());
 app.use(morgan('dev'));
 
